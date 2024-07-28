17aa69ed4ec028ca1715636c4f8b9f23706bcb2c
Gino Paoletti <ginopaoletti.dev@gmail.com>
2024-07-28 20:18:48 -0300

    project setup

---
+++ src/test/java/com/gipage/laundryapp/LaundryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gipage.laundryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
